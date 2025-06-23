1288cf9183bef7bfb632f143274e9090c9e4c4c1
minhkhoiv12 <khoitk14033@gmail.com>
2025-06-23 20:10:23 +0700

    Upload Banner

---
+++ dashboard/lib/controllers/banner_controller.dart
@@ -0,0 +1,36 @@
+
+import 'package:cloudinary_public/cloudinary_public.dart';
+import 'package:dashboard_ecomerce/global_variables.dart';
+import 'package:dashboard_ecomerce/models/banner.dart';
+import 'package:dashboard_ecomerce/services/manager_http_response.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+class BannerController {
+  uploadCategory({required dynamic pickedImage, required BuildContext context,})async {
+    try {
+     final cloudinary = CloudinaryPublic('dqtjhtikp', 'upload1');
+     CloudinaryResponse imageResponse = await cloudinary.uploadFile(
+      CloudinaryFile.fromBytesData(pickedImage, identifier: 'pickedImage', folder: 'banners'),
+     );
+//print(imageResponse);
+      String image = imageResponse.secureUrl;
+      BannerModel bannerModel = BannerModel(id: '', image: image);
+      http.Response response = await http.post(Uri.parse('$uri/api/banner'),
+        body: bannerModel.toJson(),// Convert the User object to JSON
+        headers: <String, String>{
+          // Set the content type to application/json
+          'Content-Type': 'application/json; charset=UTF-8', // specify the content type as Json
+       }
+      );
+      manageHttpResponse(response: response, context: context, onSuccess: (){
+        //  Navigator.push(context, MaterialPageRoute(builder: (context) => const CategoryScreen()));
+       
+        showSnackBar(context, 'Tải banner lên thành công');
+      });
+
+    }
+    catch(e){
+      print('Lỗi upload lên cloudinary: $e');
+    }
+  }
+}
--- dashboard/lib/views/side_bar_screen/upload_banner_screen.dart
+++ dashboard/lib/views/side_bar_screen/upload_banner_screen.dart
@@ -1,15 +1,88 @@
+import 'package:dashboard_ecomerce/controllers/banner_controller.dart';
+import 'package:file_picker/file_picker.dart';
 import 'package:flutter/material.dart';
 
-class UploadBanner extends StatelessWidget {
+class UploadBanner extends StatefulWidget {
    static const String id = '\banner-screen'; 
   const UploadBanner({super.key});
 
   @override
-  Widget build(BuildContext context) {
-     return Center(
-      child: Text(
-        "Upload Banner Screen",
-      ),
+  State<UploadBanner> createState() => _UploadBannerState();
+}
+
+class _UploadBannerState extends State<UploadBanner> {
+  final BannerController _bannerController = BannerController();
+   dynamic _image;
+  pickImage() async {
+    FilePickerResult? result = await FilePicker.platform.pickFiles(
+      type: FileType.image,
+      allowMultiple: false,
     );
+    if( result !=null) {
+      setState(() {
+        _image = result.files.first.bytes;
+      });
+    }
+  }
+  @override
+  Widget build(BuildContext context) {
+     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            alignment: Alignment.topLeft,
+            child: Text(
+              "Banners",
+              style: TextStyle(
+                fontSize: 36,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+        Divider(
+          color: Colors.grey,
+          thickness: 2,
+        ),
+        Row(
+          children: [
+            Container(
+              width: 150,
+              height: 150,
+              decoration: BoxDecoration(
+                color: Colors.grey,
+                borderRadius: BorderRadius.circular(5,),
+              ),
+              child: Center(
+                child: _image !=null ? Image.memory(_image): const Text("Thêm banner",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: ElevatedButton(
+              onPressed: () async{
+                await _bannerController.uploadCategory(pickedImage: _image, context: context);
+              }, 
+              child: Text("Lưu"),
+              ),
+            ),
+
+          ],
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ElevatedButton(
+            onPressed: () {
+              pickImage();
+            }, 
+            child: Text('Thêm hình ảnh'),
+            ),
+        ),
+      ],
+     );
   }
 }
