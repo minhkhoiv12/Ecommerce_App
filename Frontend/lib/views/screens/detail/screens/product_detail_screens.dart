a9c75f9ab03cb61ddf1f1153c2fc5a5cb419dfab
minhkhoiv12 <khoitk14033@gmail.com>
2025-08-08 18:04:00 +0700

    Product Detail Screen

---
+++ Frontend/lib/views/screens/detail/screens/product_detail_screens.dart
@@ -0,0 +1,171 @@
+import 'package:bai1/models/product.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ProductDetailScreens extends StatefulWidget {
+  //const ProductDetailScreens({super.key});
+  final Product product;
+
+  const ProductDetailScreens({super.key, required this.product}); 
+  @override
+  State<ProductDetailScreens> createState() => _ProductDetailScreensState();
+}
+
+class _ProductDetailScreensState extends State<ProductDetailScreens> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Chi tiết sản phẩm',
+          style:  GoogleFonts.quicksand(
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        centerTitle: true,
+        actions: [
+          IconButton(onPressed: (){}, icon:const Icon(Icons.favorite_border),
+          ),
+        ],
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Center(
+            child: Container(
+              width: 260,
+              height: 275,
+              clipBehavior: Clip.hardEdge,
+              decoration: const BoxDecoration(
+                
+              ),
+              child: Stack(
+                clipBehavior: Clip.none,
+                children: [
+                  Positioned(
+                    left: 0,
+                    top: 50,
+                    child: Container(
+                      width: 260,
+                      height: 260,
+                      clipBehavior: Clip.hardEdge,
+                      decoration: BoxDecoration(
+                        color: const Color(0xFFD8DDFF),
+                        borderRadius: BorderRadius.circular(130),
+                      ),
+                    ), 
+                  ),
+                  Positioned(
+                    left: 22,
+                    top: 0,
+                    child: Container(
+                      width: 216,
+                      height: 274,
+                      clipBehavior: Clip.hardEdge,
+                      decoration: BoxDecoration(
+                        color: const Color(0xFF9CA8FF),
+                        borderRadius: BorderRadius.circular(14),
+                      ),
+                      child: SizedBox(
+                        height: 300,
+                        child: PageView.builder(
+                          itemCount: widget.product.images.length,
+                          scrollDirection: Axis.horizontal,
+                          itemBuilder: (context, index){
+                            return Image.network(widget.product.images[index],
+                              width: 198,
+                              height: 22,
+                              fit: BoxFit.cover,
+                            );
+                          }),
+                      )
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(widget.product.productName,
+                  style: GoogleFonts.roboto(
+                    fontSize: 17,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 1,
+                    color:const Color(0xFF3C55Ef),
+                  ),
+                ),
+                Text("\$${widget.product.productPrice}",
+                  style: GoogleFonts.roboto(
+                    fontSize: 17,
+                    fontWeight: FontWeight.bold,
+                    color:const Color(0xFF3C55Ef),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Text(widget.product.category,
+              style: GoogleFonts.roboto(
+                color: Colors.grey,
+                fontSize: 16,
+                fontWeight: FontWeight.w700,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Về",
+                  style: GoogleFonts.lato(
+                    fontSize: 17,
+                    letterSpacing: 1.7,
+                    color:const Color(0xFF363330),
+                  ),
+                ),
+                Text(widget.product.description,
+                  style: GoogleFonts.mochiyPopOne(
+                    letterSpacing: 2,
+                    fontSize: 15,
+                  ),
+                ), 
+              ],
+              
+            ),
+    
+          )
+        ],
+      ),
+      bottomSheet: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: InkWell(
+          onTap: (){},
+          child: Container(
+            height: 46,
+            width: 386,
+            clipBehavior: Clip.hardEdge,
+            decoration: BoxDecoration(
+              color: const Color(0xFF3B54EE),
+              borderRadius: BorderRadius.circular(15),
+            ),
+            child: Center(
+              child: Text("Thêm vào giỏ hàng",
+              style: GoogleFonts.mochiyPopOne(
+                fontSize: 16,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
--- Frontend/lib/views/screens/nav_screens/widgets/product_item_widget.dart
+++ Frontend/lib/views/screens/nav_screens/widgets/product_item_widget.dart
@@ -1,4 +1,5 @@
 import 'package:bai1/models/product.dart';
+import 'package:bai1/views/screens/detail/screens/product_detail_screens.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
@@ -10,61 +11,68 @@ class ProductItemWidget extends StatelessWidget {
   
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: 170,
-      margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 10),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            height: 170,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(10),
-              color: const Color(0xffF2F2F2),
-            ),
-            child: Stack(
-              children: [
-                Image.network(product.images[0], height: 170,
-                fit: BoxFit.cover,
-                ),
-                Positioned(
-                  top: 15,
-                  right: 2,
-                  child: Image.asset(
-                    'assets/icons/love.png',
-                    width: 20,
-                    height: 20,
+    return InkWell(
+      onTap: (){
+        Navigator.push(context, MaterialPageRoute(builder: (context){
+          return ProductDetailScreens(product: product);
+        }));
+      },
+      child: Container(
+        width: 170,
+        margin: const EdgeInsets.symmetric(horizontal: 8, vertical: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              height: 170,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                color: const Color(0xffF2F2F2),
+              ),
+              child: Stack(
+                children: [
+                  Image.network(product.images[0], height: 170,
+                  fit: BoxFit.cover,
                   ),
-                ),
-                Positioned(
-                  bottom: 0,
-                  right: 0,
-                  child: Image.asset('assets/icons/cart.png',
-                  width: 26,
-                  height: 26,),
-                )
-              ],
+                  Positioned(
+                    top: 15,
+                    right: 2,
+                    child: Image.asset(
+                      'assets/icons/love.png',
+                      width: 20,
+                      height: 20,
+                    ),
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    right: 0,
+                    child: Image.asset('assets/icons/cart.png',
+                    width: 26,
+                    height: 26,),
+                  )
+                ],
+              ),
             ),
-          ),
-          SizedBox(height: 8),
-          Text(
-            product.productName,
-            overflow: TextOverflow.ellipsis,
-            style: GoogleFonts.roboto(
-              fontSize: 13,
-              fontWeight: FontWeight.bold,
-              color: Color(0xFF212121),
+            SizedBox(height: 8),
+            Text(
+              product.productName,
+              overflow: TextOverflow.ellipsis,
+              style: GoogleFonts.roboto(
+                fontSize: 13,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF212121),
+              ),
             ),
-          ),
-          const SizedBox(height: 4),
-          Text(product.category,
-            style: GoogleFonts.quicksand(
-              fontSize: 13,
-              fontWeight: FontWeight.bold,
-              color: Color(0xFF757575),
+            const SizedBox(height: 4),
+            Text(product.category,
+              style: GoogleFonts.quicksand(
+                fontSize: 13,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF757575),
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
